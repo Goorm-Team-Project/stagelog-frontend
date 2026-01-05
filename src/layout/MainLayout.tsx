601c57686cae8150ae478bb27c60beb9b65f59d8
ysheep0906 <wlq94814@naver.com>
2026-01-05 16:17:12 +0900

    feat: integrate header, hero section, and homepage into layout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Header from '@/components/Header'
+import HeroSection from '@/components/HeroSection'
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-
-      <main className="flex-1 container py-6">
+      <HeroSection />
+      <main className="flex-1 py-6">
         <Outlet />
       </main>
     </div>
