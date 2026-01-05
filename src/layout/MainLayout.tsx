4d0f185f29ed753f3b47d9b1aa36f970f4c28384
ysheep0906 <wlq94814@naver.com>
2026-01-05 16:30:24 +0900

    feat:integrate footer into layout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Header from '@/components/Header'
 import HeroSection from '@/components/HeroSection'
+import Footer from '@/components/Footer'
 
 export default function MainLayout() {
   return (
@@ -10,6 +11,7 @@ export default function MainLayout() {
       <main className="flex-1 py-6">
         <Outlet />
       </main>
+      <Footer />
     </div>
   )
 }
