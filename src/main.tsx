ebbe5e58b0c9cecd7dce1a94932bb06d7679ec79
ysheep0906 <wlq94814@naver.com>
2026-01-12 16:29:34 +0900

    feat: strict Mode 개발 환경에서 OFF

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
+import '@/services/interceptor'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>,
+  // </StrictMode>,
 )
